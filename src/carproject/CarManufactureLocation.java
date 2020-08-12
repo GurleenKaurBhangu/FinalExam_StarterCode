f85c77980925f648246ccb2134bf20fd8db9e171
sivagamasheridan <46792094+sivagamasheridan@users.noreply.github.com>
2020-08-12 10:02:05 -0400

    Update CarManufactureLocation.java

--- src/carproject/CarManufactureLocation.java
+++ src/carproject/CarManufactureLocation.java
@@ -10,7 +10,7 @@ package carproject;
  * cars to ensure consistency.
  * Users can order the number of car they want to buy
  * @author Sivagama Srinivasan
-  April ,2020 */
+  August,2020 */
 public class CarManufactureLocation {
    
   public int numofcars = 0;// how many orders to manufacture car
